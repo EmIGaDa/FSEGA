6b9de11bdad50fad4e19b6e7bbc2483d98b52386
Emanuel Gadalean <egadalean2422@gmail.com>
2023-01-17 16:33:02 +0200

    add validations

--- Proiect/Models/Doctor.cs
+++ Proiect/Models/Doctor.cs
@@ -1,4 +1,5 @@
 ﻿using SQLite;
+using System.ComponentModel.DataAnnotations;
 
 namespace Proiect.Models
 {
@@ -14,10 +15,15 @@ namespace Proiect.Models
     {
         public int DoctorId { get; set; }
 
+        [StringLength(50)]
         public string Name { get; set; }
+
+        [Range(23, 80)]
         public int Age { get; set; }
         public Function Function { get; set; }
 
+        [RegularExpression("^[A-Za-z0-9._%+-]*@[A-Za-z0-9.-]*\\.[A-Za-z0-9-]{2,}$",
+            ErrorMessage = "Email is required and must be properly formatted.")]
         public string Email { get; set; }
 
         public ICollection<Appointment>? Appointments { get; set; }
--- Proiect/Models/Patient.cs
+++ Proiect/Models/Patient.cs
@@ -1,12 +1,23 @@
-﻿namespace Proiect.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proiect.Models
 {
     public class Patient
     {
 
         public int PatientId { get; set; }
+
+        [StringLength(50)]
         public string Name { get; set; }
+
+        [Range(1, 105)]
         public int Age { get; set; }
+
+        [MinLength(13, ErrorMessage = "CNP must contain 13 numbers"), MaxLength(13, ErrorMessage = "CNP must contain 13 numbers")]
         public long CNP { get; set; }
+
+        [RegularExpression("^[A-Za-z0-9._%+-]*@[A-Za-z0-9.-]*\\.[A-Za-z0-9-]{2,}$",
+            ErrorMessage = "Email is required and must be properly formatted.")]
         public string Email { get; set; }
 
         public ICollection<Appointment>? Appointments { get; set; }
